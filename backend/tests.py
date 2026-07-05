a867c63f6764fdc4ea9d95f078e2c4175ce8abf4
Kamilek7 <kamijar20031@gmail.com>
2026-07-06 00:29:58 +0200

    please just work

--- backend/tests.py
+++ backend/tests.py
@@ -1,9 +1,9 @@
 import pytest
 import pandas as pd
 from harmoHelpers import *
-import datetime
 
 def loadCases():
+    import datetime
     df = pd.read_excel("tests/test_daily.ods", engine="odf")
     rowKeys = {"A" : {"interval" : 1, "start": "2026-06-06", "lastAdded" : "2026-06-05"}, "B" : {"interval" : 2, "start": "2026-06-06", "lastAdded" : "2026-06-05"}, "C" : {"interval" : 3, "start": "2026-06-06", "lastAdded" : "2026-06-05"}, "D" : {"interval" : 5, "start": "2026-06-06", "lastAdded" : "2026-06-20"}, "E" : {"interval" : 7, "start": "2026-06-06", "lastAdded" : "2026-06-05"}, 
     "F" : {"interval" : 1, "start": "2026-06-12", "lastAdded" : "2026-06-05"}, "G" : {"interval" : 2, "start": "2026-06-12", "lastAdded" : "2026-06-05"}, "H" : {"interval" : 3, "start": "2026-06-12", "lastAdded" : "2026-06-05"}, "I" : {"interval" : 5, "start": "2026-06-12", "lastAdded" : "2026-06-05"}, "J" : {"interval" : 7, "start": "2026-06-12", "lastAdded" : "2026-06-05"}}
